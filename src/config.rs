4c0b75533fb019b02a2e398c8538925b67772693
totti2202344 <totti2202344@hotmail.com>
2025-09-01 08:26:32 +0800

    修改服务器key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nextcloud.francessco.top"];
+pub const RS_PUB_KEY: &str = "5j02rS+gq4jtBWynFr95aAXjcBOqS8Onj5LLaaxvhQ8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
